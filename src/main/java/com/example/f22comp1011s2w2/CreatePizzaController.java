02344a92cc3c8c828e4d28efc745780de54f4f2b
JaretWright <jaret.wright@georgiancollege.ca>
2022-09-14 13:30:10 -0400

    Lesson 2 - styled the create pizza view and updated the controller with the fx:id's

--- src/main/java/com/example/f22comp1011s2w2/CreatePizzaController.java
+++ src/main/java/com/example/f22comp1011s2w2/CreatePizzaController.java
@@ -1,4 +1,77 @@
 package com.example.f22comp1011s2w2;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.RadioButton;
+
 public class CreatePizzaController {
+
+    @FXML
+    private CheckBox baconCheckBox;
+
+    @FXML
+    private CheckBox cornCheckBox;
+
+    @FXML
+    private Label costPizzaLabel;
+
+    @FXML
+    private RadioButton deepDishRadioButton;
+
+    @FXML
+    private CheckBox deliveryCheckBox;
+
+    @FXML
+    private CheckBox greenPepperCheckBox;
+
+    @FXML
+    private CheckBox jalapenoCheckBox;
+
+    @FXML
+    private CheckBox mushroomsCheckBox;
+
+    @FXML
+    private CheckBox olivesCheckBox;
+
+    @FXML
+    private CheckBox onionCheckBox;
+
+    @FXML
+    private CheckBox pepperoniCheckBox;
+
+    @FXML
+    private CheckBox pineappleCheckBox;
+
+    @FXML
+    private ComboBox<?> pizzaSizeComboBox;
+
+    @FXML
+    private CheckBox redPepperCheckBox;
+
+    @FXML
+    private RadioButton regDoughRadioButton;
+
+    @FXML
+    private RadioButton regularCrustRadioButton;
+
+    @FXML
+    private ComboBox<?> sauceComboBox;
+
+    @FXML
+    private CheckBox sausageCheckBox;
+
+    @FXML
+    private Label taxLabel;
+
+    @FXML
+    private RadioButton thinCrustRadioButton;
+
+    @FXML
+    private Label totalLabel;
+
+    @FXML
+    private RadioButton wholeWheatRadioButton;
+
 }
--- src/main/java/com/example/f22comp1011s2w2/Pizza.java
+++ src/main/java/com/example/f22comp1011s2w2/Pizza.java
@@ -28,7 +28,7 @@ public class Pizza {
 
     public static List<String> validSizes()
     {
-        return Arrays.asList("small", "medium", "large");
+        return Arrays.asList("small", "medium", "large","x-large");
     }
 
     /**
