abd6742d90fe1bb1c3d17431cf15d65a186914cf
JaretWright <jaret.wright@georgiancollege.ca>
2022-09-28 12:07:54 -0400

    Week 4 - added INSERT commands to SQL file

--- create tables.sql
+++ create tables.sql
@@ -26,3 +26,24 @@ CREATE TABLE toppingsOnPizza
     FOREIGN KEY (toppingID) REFERENCES toppings(toppingID)
 );
 
+INSERT INTO toppings (toppingName) VALUES ('cheese'),('olives'),('pepperoni'),
+										('anchovies'),('pineapple');
+									
+SELECT * FROM toppings;
+
+INSERT INTO pizzas (dough, crustStyle, sauce, delivery, price) VALUES
+	('whole wheat','thin','tomato',true,35.99);
+
+SELECT * FROM pizzas;
+
+INSERT INTO toppingsOnPizza (pizzaID, toppingID) VALUES (1,1);
+INSERT INTO toppingsOnPizza (pizzaID, toppingID) VALUES (1,2);
+INSERT INTO toppingsOnPizza (pizzaID, toppingID) VALUES (1,3);
+INSERT INTO toppingsOnPizza (pizzaID, toppingID) VALUES (1,4);
+INSERT INTO toppingsOnPizza (pizzaID, toppingID) VALUES (1,5);
+
+SELECT * FROM pizzas
+INNER JOIN toppingsOnPizza
+INNER JOIN toppings
+WHERE pizzas.pizzaID = toppingsOnPizza.pizzaID && 
+		toppingsOnPizza.toppingID = toppings.toppingID;
